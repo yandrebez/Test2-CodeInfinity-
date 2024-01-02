f12298fbccf18fb6f9423a6c5be435336c5989b5
yandrebez <yandrebez007@gmail.com>
2024-01-02 10:29:54 +0200

    Variable changes

--- uploadf.php
+++ uploadf.php
@@ -9,13 +9,13 @@ require_once __DIR__ . '/vendor/autoload.php';
 try {
     // Check for POST method
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $db = new PDO('sqlite:C:\Users\27837\test2.db');
+        $dataBase = new PDO('sqlite:C:\Users\27837\test2.db');
         echo 'Connection was successful';
 
         $tableName = 'csv_import';
 
         // Set the PDO attribute to throw exceptions on errors
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $dataBase->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         // Define SQL statement to create the table
         $createTableSQL = "
@@ -30,20 +30,21 @@ try {
         ";
 
         // Execute the SQL statement to create the table
-        $db->exec($createTableSQL);
+        $dataBase->exec($createTableSQL);
         echo 'Table created successfully';
 
-        // Clear database on new csv upload
+        //Making sure db is clear for upload of csv
         $clearTable = [$tableName];
 
         foreach ($clearTable as $clearTable) {
-            $db->exec("DELETE FROM $clearTable");
+            $dataBase->exec("DELETE FROM $clearTable");
         }
 
         // Check if a file was uploaded
         if (isset($_FILES['csvFile']) && $_FILES['csvFile']['error'] == UPLOAD_ERR_OK) {
             $csvFilePath = $_FILES['csvFile']['tmp_name'];
 
+            //Im deviding the csv into Chunk sizes of 10000 rows per chunk
             $csvChunks = array_chunk(file($csvFilePath), 10000);
 
             // Prepare the SQL statement
@@ -52,10 +53,9 @@ try {
                 VALUES (?, ?, ?, ?, ?, ?);
             ";
 
-            $stmt = $db->prepare($insertDataSQL);
+            $statement = $dataBase->prepare($insertDataSQL);
 
-            // Use a transaction for improved efficiency
-            $db->beginTransaction();
+            $dataBase->beginTransaction();
 
             foreach ($csvChunks as $chunk) {
                 foreach ($chunk as $row) {
@@ -64,19 +64,19 @@ try {
                     }
                     $data = str_getcsv($row);
 
-                    $stmt->bindParam(1, $data[0], PDO::PARAM_STR);
-                    $stmt->bindParam(2, $data[1], PDO::PARAM_STR);
-                    $stmt->bindParam(3, $data[2], PDO::PARAM_STR);
-                    $stmt->bindParam(4, $data[3], PDO::PARAM_STR);
-                    $stmt->bindParam(5, $data[4], PDO::PARAM_STR);
-                    $stmt->bindParam(6, $data[5], PDO::PARAM_STR);
+                    $statement->bindParam(1, $data[0], PDO::PARAM_STR);
+                    $statement->bindParam(2, $data[1], PDO::PARAM_STR);
+                    $statement->bindParam(3, $data[2], PDO::PARAM_STR);
+                    $statement->bindParam(4, $data[3], PDO::PARAM_STR);
+                    $statement->bindParam(5, $data[4], PDO::PARAM_STR);
+                    $statement->bindParam(6, $data[5], PDO::PARAM_STR);
 
-                    $stmt->execute();
+                    $statement->execute();
                 }
             }
 
             // Commit the transaction
-            $db->commit();
+            $dataBase->commit();
 
             echo "Data inserted successfully.";
         } else {
